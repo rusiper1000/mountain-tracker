e6a49e645740d8413165524318f0305c45506e8e
rusiper1000 <rusiper1000@gmail.com>
2026-03-12 19:41:19 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'mountain-v93-cache';
+const CACHE_NAME = 'mountain-v92-cache';
 const APP_SHELL = [
-  './index.html?v=v93',
-  './manifest.webmanifest?v=v93',
-  './icon-192.png?v=v93',
-  './icon-512.png?v=v93'
+  './index.html?v=v92',
+  './manifest.webmanifest?v=v92',
+  './icon-192.png?v=v92',
+  './icon-512.png?v=v92'
 ];
 self.addEventListener('install', event => {
   self.skipWaiting();
@@ -22,9 +22,9 @@ self.addEventListener('fetch', event => {
   if (req.mode === 'navigate') {
     event.respondWith(fetch(req).then(res => {
       const copy = res.clone();
-      caches.open(CACHE_NAME).then(cache => cache.put('./index.html?v=v93', copy)).catch(() => {});
+      caches.open(CACHE_NAME).then(cache => cache.put('./index.html?v=v92', copy)).catch(() => {});
       return res;
-    }).catch(() => caches.match('./index.html?v=v93')));
+    }).catch(() => caches.match('./index.html?v=v92')));
     return;
   }
   event.respondWith(caches.match(req).then(cached => cached || fetch(req).then(res => {
