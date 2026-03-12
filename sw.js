d8317587cd8f18f5d7c6d4dbe3a6c2ef055e616f
rusiper1000 <rusiper1000@gmail.com>
2026-03-12 19:20:56 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,36 +1,35 @@
-const CACHE_NAME = 'mountain-v90-cache-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './icon-192.png',
-  './icon-512.png'
+const CACHE_NAME = 'mountain-v92-cache';
+const APP_SHELL = [
+  './index.html?v=v92',
+  './manifest.webmanifest?v=v92',
+  './icon-192.png?v=v92',
+  './icon-512.png?v=v92'
 ];
-
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).catch(() => {}));
 });
-
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(key => {
-      if (key !== CACHE_NAME) return caches.delete(key);
-    })))
-  );
-  self.clients.claim();
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
+    await self.clients.claim();
+  })());
 });
-
 self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      if (cached) return cached;
-      return fetch(event.request).then(response => {
-        const clone = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone)).catch(() => {});
-        return response;
-      }).catch(() => caches.match('./index.html'));
-    })
-  );
+  const req = event.request;
+  if (req.method !== 'GET') return;
+  if (req.mode === 'navigate') {
+    event.respondWith(fetch(req).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put('./index.html?v=v92', copy)).catch(() => {});
+      return res;
+    }).catch(() => caches.match('./index.html?v=v92')));
+    return;
+  }
+  event.respondWith(caches.match(req).then(cached => cached || fetch(req).then(res => {
+    const copy = res.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(req, copy)).catch(() => {});
+    return res;
+  }).catch(() => cached)));
 });
